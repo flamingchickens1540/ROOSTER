ebbbd50474a30a110b60f4349e8c0388a0cb8270
RobinsonZ <robinsonz@catlin.edu>
2017-12-22 14:23:20 -0800

    Create SubsystemAttached interface
    This interface gets around some of the problems with multiple
    inheritance by requiring all sub-implementors to provide a Subsystem
    instance. This will produce some rather unsightly boilerplate but it's
    better than nothing. We could even use a default method that checks if
    the implementor is an instance of Subsystem and throws an exception if
    it isn't.

---
+++ src/main/java/org/team1540/base/templates/SubsystemAttached.java
@@ -0,0 +1,20 @@
+package org.team1540.base.templates;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ * Interface for classes that should be Subsystems. Super-interface to all template classes.
+ */
+public interface SubsystemAttached {
+  /**
+   * Gets a subsystem instance related to this class. In almost all cases, the class extending this
+   * interface is a subsystem anyway, and in that case the implementation will literally just be
+   * "{@code return this;}". Theoretically you could have more complicated implementations but
+   * there's no good reason to. You'll usually be implementing this through a sub-interface such as
+   * {@link Drive} which are all for subsystem-like things anyway.
+   *
+   * @return A subsystem instance. The result of this method should be equal across all invocations
+   *     of this method in one program. Or, in English, this should return a constant value.
+   */
+  public Subsystem getSubsystem();
+}
