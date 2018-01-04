6dc8d6b68ca80c05cd072292a3940731a04bfaa5
edelmanjm <edelmanjm@users.noreply.github.com>
2018-01-03 16:58:15 -0800

    Add getters and setters for motors

--- src/main/java/org/team1540/base/ChickenSubsystem.java
+++ src/main/java/org/team1540/base/ChickenSubsystem.java
@@ -2,6 +2,7 @@ package org.team1540.base;
 
 import com.ctre.CANTalon;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 import org.team1540.base.power.PowerManageable;
@@ -11,10 +12,48 @@ import org.team1540.base.power.PowerManageable;
  * Simple implementation of core {@link Subsystem} related interfaces. Makes it quick and easy to
  * build a basic robot.
  */
+@SuppressWarnings("unused")
 public class ChickenSubsystem extends Subsystem implements PowerManageable {
 
   private double priority = 0.0;
-  private final Set<CANTalon> motors = new HashSet<CANTalon>();
+
+  private final Set<CANTalon> motors = new HashSet<>();
+
+  public int size() {
+    return motors.size();
+  }
+
+  public boolean isEmpty() {
+    return motors.isEmpty();
+  }
+
+  public boolean contains(CANTalon o) {
+    return motors.contains(o);
+  }
+
+  public boolean add(CANTalon canTalon) {
+    return motors.add(canTalon);
+  }
+
+  public boolean remove(CANTalon o) {
+    return motors.remove(o);
+  }
+
+  public boolean containsAll(Collection<CANTalon> c) {
+    return motors.containsAll(c);
+  }
+
+  public boolean addAll(Collection<? extends CANTalon> c) {
+    return motors.addAll(c);
+  }
+
+  public boolean removeAll(Collection<CANTalon> c) {
+    return motors.removeAll(c);
+  }
+
+  public void clear() {
+    motors.clear();
+  }
 
   private final Object powerLock = new Object();
 
