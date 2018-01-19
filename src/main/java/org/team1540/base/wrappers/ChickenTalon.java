747eb4d3bd4d072e99fa7fde8d456deea1bbfe84
edelmanjm <edelmanjm@users.noreply.github.com>
2018-01-18 16:50:43 -0800

    Fix semicolon in JavaDoc

--- src/main/java/org/team1540/base/wrappers/ChickenTalon.java
+++ src/main/java/org/team1540/base/wrappers/ChickenTalon.java
@@ -1024,7 +1024,7 @@ public class ChickenTalon extends TalonSRX {
    * Select which closed loop profile to use, and uses whatever PIDF gains and the such that are
    * already there.
    *
-   * @deprecated Use {@link #selectProfileSlot(int);}
+   * @deprecated Use {@link #selectProfileSlot(int)}
    */
   @Deprecated
   public void setProfile(int profile) {
