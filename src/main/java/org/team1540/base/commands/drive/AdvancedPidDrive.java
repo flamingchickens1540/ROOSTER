17962485ea3e6856b203ceb34e8d17ad0249d2c4
Zachary Robinson <robinsonz@catlin.edu>
2018-01-28 21:49:15 -0800

    Add constructor and brownout protection mode

--- src/main/java/org/team1540/base/commands/drive/AdvancedPidDrive.java
+++ src/main/java/org/team1540/base/commands/drive/AdvancedPidDrive.java
@@ -2,14 +2,24 @@ package org.team1540.base.commands.drive;
 
 import static java.lang.Math.abs;
 import static org.team1540.base.Utilities.constrain;
+import static org.team1540.base.Utilities.processAxisDeadzone;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.PowerDistributionPanel;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Command;
 import org.team1540.base.templates.CtreDrive;
+import org.team1540.base.triggers.VibrationManager;
 import org.team1540.base.wrappers.ChickenController;
 
 public class AdvancedPidDrive extends Command {
+
+  // pdp object for easy access
+  private final PowerDistributionPanel pdp = new PowerDistributionPanel();
+
+  // rumble command
+  private final Command rumbleCommand;
 
   // settings
   private CtreDrive drive;
@@ -33,36 +43,95 @@ public class AdvancedPidDrive extends Command {
 
   private double deadzone;
 
+  private boolean usingBrownoutAlert;
+  private double maxBrownoutCooldown;
+
+  // algo fields
+  private boolean inBrownoutCooldown = false;
+  private Timer brownoutCooldownTimer = new Timer();
+
+  public AdvancedPidDrive(CtreDrive drive, double maxSetpoint,
+      Joystick joystick, int leftAxis, int rightAxis, int fwdTrigger, int backTrigger,
+      boolean invertLeftAxis, boolean invertRightAxis, boolean invertLeftOutput,
+      boolean invertRightOutput, boolean invertFwdTrigger, boolean invertBackTrigger,
+      double maxBrakePct, boolean minMotorOutput, double deadzone, boolean usingBrownoutAlert,
+      double maxBrownoutCooldown) {
+    this.drive = drive;
+    this.maxSetpoint = maxSetpoint;
+    this.joystick = joystick;
+    this.leftAxis = leftAxis;
+    this.rightAxis = rightAxis;
+    this.fwdTrigger = fwdTrigger;
+    this.backTrigger = backTrigger;
+    this.invertLeftAxis = invertLeftAxis;
+    this.invertRightAxis = invertRightAxis;
+    this.invertLeftOutput = invertLeftOutput;
+    this.invertRightOutput = invertRightOutput;
+    this.invertFwdTrigger = invertFwdTrigger;
+    this.invertBackTrigger = invertBackTrigger;
+    this.maxBrakePct = maxBrakePct;
+    this.minMotorOutput = minMotorOutput;
+    this.deadzone = deadzone;
+    this.usingBrownoutAlert = usingBrownoutAlert;
+    this.maxBrownoutCooldown = maxBrownoutCooldown;
+    rumbleCommand = VibrationManager.getVibrationCommand(joystick, maxBrownoutCooldown, 1);
+  }
 
   @Override
   protected void execute() {
 
-    // find the setpoint taking into account triggers and inversions
-    double forwardAdd = joystick.getRawAxis(fwdTrigger) * (invertFwdTrigger ? -1 : 1);
-    double backAdd = joystick.getRawAxis(backTrigger) * (invertBackTrigger ? -1 : 1);
-    double triggers = forwardAdd - backAdd;
+    if (!inBrownoutCooldown) {
+      // find the setpoint taking into account triggers and inversions
+      double forwardAdd = joystick.getRawAxis(fwdTrigger) * (invertFwdTrigger ? -1 : 1);
+      double backAdd = joystick.getRawAxis(backTrigger) * (invertBackTrigger ? -1 : 1);
+      double triggers = forwardAdd - backAdd;
 
-    double leftSetpoint = constrain(
-        (joystick.getRawAxis(leftAxis) * (invertLeftAxis ? -1 : 1)
-            + triggers) * (invertLeftOutput ? -1 : 1),
-        1);
-    double rightSetpoint = constrain(
-        (joystick.getRawAxis(rightAxis) * (invertRightAxis ? -1 : 1)
-            + triggers) * (invertRightOutput ? -1 : 1),
-        1);
+      double leftSetpoint = constrain(
+          (joystick.getRawAxis(leftAxis) * (invertLeftAxis ? -1 : 1)
+              + triggers) * (invertLeftOutput ? -1 : 1),
+          1);
+      double rightSetpoint = constrain(
+          (joystick.getRawAxis(rightAxis) * (invertRightAxis ? -1 : 1)
+              + triggers) * (invertRightOutput ? -1 : 1),
+          1);
 
-    // store these in variables for easy acess
-    ChickenController left = drive.getLeftMaster();
-    ChickenController right = drive.getRightMaster();
+      // store these in variables for easy acess
+      ChickenController left = drive.getLeftMaster();
+      ChickenController right = drive.getRightMaster();
 
-    // prevent backdriving above a configured brake amount
+      processPeakOutput(left);
+      processPeakOutput(right);
 
-    // figure out if there is any motor movement
-    processPeakOutput(left);
-    processPeakOutput(right);
+      if (usingBrownoutAlert) {
+        // check if the RIO is browning out. If so, set the motors to neutral and vibrate the controller
+        // until the joysticks are released / some time has passed
+        if (pdp.getVoltage() < 6.8) {
+          // we're browning out (first-stage brownout triggers at 6.8 volts per
+          // https://wpilib.screenstepslive.com/s/4485/m/cs_hardware/l/289498-roborio-brownout-and-understanding-current-draw#Stage1-OutputDisable)
+          brownoutCooldownTimer.reset();
+          brownoutCooldownTimer.start();
+          inBrownoutCooldown = true;
+          left.set(ControlMode.PercentOutput, 0);
+          right.set(ControlMode.PercentOutput, 0);
+          rumbleCommand.start();
+          return;
+        }
+      }
 
-    left.set(ControlMode.Velocity, constrain(leftSetpoint, 1));
-    right.set(ControlMode.Velocity, constrain(rightSetpoint, 1));
+      left.set(ControlMode.Velocity, constrain(leftSetpoint, 1));
+      right.set(ControlMode.Velocity, constrain(rightSetpoint, 1));
+    } else {
+      if (brownoutCooldownTimer.hasPeriodPassed(maxBrownoutCooldown)
+          || (processAxisDeadzone(joystick.getRawAxis(leftAxis), deadzone) == 0
+          && processAxisDeadzone(joystick.getRawAxis(rightAxis), deadzone) == 0
+          && processAxisDeadzone(joystick.getRawAxis(fwdTrigger), deadzone) == 0
+          && processAxisDeadzone(joystick.getRawAxis(backTrigger), deadzone) == 0)
+          ) {
+        brownoutCooldownTimer.stop();
+        inBrownoutCooldown = false;
+        rumbleCommand.cancel();
+      }
+    }
   }
 
   private void processPeakOutput(ChickenController controller) {
