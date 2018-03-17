984de207cb58e75247fb200d44e1353f21db7d39
Zachary Robinson <robinsonz@catlin.edu>
2018-03-17 16:54:56 -0700

    Revise Executable documentation to be generic
    Signed-off-by: Zachary Robinson <robinsonz@catlin.edu>

--- src/main/java/org/team1540/base/util/Executable.java
+++ src/main/java/org/team1540/base/util/Executable.java
@@ -1,7 +1,8 @@
 package org.team1540.base.util;
 
 /**
- * Interface for actions that can be passed to a {@link SimpleCommand}.
+ * Interface for simple no-argument, no-return-value actions such as those used by a
+ * {@link SimpleCommand}.
  */
 @FunctionalInterface
 public interface Executable {
