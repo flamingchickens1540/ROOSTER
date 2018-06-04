09d570379fe10226281525b53ec57c1071c3c39d
Zachary Robinson <robinsonz@catlin.edu>
2018-06-04 12:19:35 -0700

    Update test robot package docs

--- lib/src/main/java/org/team1540/base/util/robots/package-info.java
+++ lib/src/main/java/org/team1540/base/util/robots/package-info.java
@@ -1,4 +1,5 @@
 /**
- * Self-contained robots to perform various checkout tasks.
+ * Self-contained robots to perform various tasks. To load one of these robots, set your
+ * robot class (in build.gradle) to org.team1540.base.util.robots.&lt;robot name&gt;.
  */
 package org.team1540.base.util.robots;
