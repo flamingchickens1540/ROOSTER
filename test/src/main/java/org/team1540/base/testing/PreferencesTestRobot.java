9a85bf5105a1b0163a28a58892785948cc2c7437
Zachary Robinson <robinsonz@catlin.edu>
2018-06-21 12:36:15 -0700

    Update PreferencesTestRobot for non-persistence

--- test/src/main/java/org/team1540/base/testing/PreferencesTestRobot.java
+++ test/src/main/java/org/team1540/base/testing/PreferencesTestRobot.java
@@ -19,6 +19,16 @@ public class PreferencesTestRobot extends IterativeRobot {
   @Preference("An int")
   public int i = 2;
 
+  // non-persistent
+  @Preference(value = "An NP boolean", persistent = false)
+  public boolean b2;
+  @Preference(value = "An NP String", persistent = false)
+  public String string2 = "String";
+  @Preference(value = "An NP double", persistent = false)
+  public double d2 = 2;
+  @Preference(value = "An NP int", persistent = false)
+  public int i2 = 2;
+
   @Override
   public void robotInit() {
     PreferenceManager.getInstance().add(this);
@@ -28,6 +38,10 @@ public class PreferencesTestRobot extends IterativeRobot {
       System.out.println("Current String value: " + string);
       System.out.println("Current double value: " + d);
       System.out.println("Current int value: " + i);
+      System.out.println("Current NP boolean value: " + b2);
+      System.out.println("Current NP String value: " + string2);
+      System.out.println("Current NP double value: " + d2);
+      System.out.println("Current NP int value: " + i2);
     }).startPeriodic(1);
   }
 
