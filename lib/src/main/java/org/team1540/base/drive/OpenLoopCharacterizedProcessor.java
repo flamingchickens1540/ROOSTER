4d680b4a7597a9920e9e25692a79e75c60db389e
Zachary Robinson <robinsonz@catlin.edu>
2018-10-15 10:23:47 -0700

    Convert CharacterizedDriveHelper to new pipeline

---
+++ lib/src/main/java/org/team1540/base/drive/OpenLoopCharacterizedProcessor.java
@@ -0,0 +1,74 @@
+package org.team1540.base.drive;
+
+
+/*
+Implementation note:
+This is not a command, as I realized that with PIDDrive so much of the configuration was simply
+dealing with scaling the joysticks and creating a full pipeline from joystick to motor controller.
+This just takes speed values in a method, and outputs a drive signal. This also means it can
+be plugged into a motion profile executor.
+*/
+
+import java.util.OptionalDouble;
+import java.util.function.Function;
+import org.jetbrains.annotations.Contract;
+import org.team1540.base.drive.pipeline.DriveData;
+import org.team1540.base.drive.pipeline.TankDriveData;
+
+public class OpenLoopCharacterizedProcessor implements Function<TankDriveData, TankDriveData> {
+
+  private final double kV;
+  private final double vIntercept;
+  private final double kA;
+
+  // TODO: Add explanation of units in class docs
+
+  /**
+   * Creates a {@code CharacterizedDriveHelper} with the provided \(k_v\) and \(v_{Intercept}\)
+   *
+   * @param kV The velocity constant feed-forward, in output units per speed unit.
+   * @param vIntercept The velocity intercept, in output units
+   * @param kA The acceleration constant feed-forward, in output units per acceleration unit.
+   */
+  public OpenLoopCharacterizedProcessor(double kV, double vIntercept, double kA) {
+    this.kV = kV;
+    this.vIntercept = vIntercept;
+    this.kA = kA;
+  }
+
+  @Contract(pure = true)
+  private OpenLoopDriveSignal getDriveSignal(double leftSpeed, double leftAccel, double rightSpeed,
+      double rightAccel) {
+    double left = getThrottle(leftSpeed, leftAccel);
+    double right = getThrottle(rightSpeed, rightAccel);
+    return new OpenLoopDriveSignal(left, right);
+  }
+
+  @Contract(pure = true)
+  private double getThrottle(double wantedSpeed, double wantedAccel) {
+    return (kV * wantedSpeed)
+        + (kA * wantedAccel)
+        + (wantedSpeed != 0 ? Math.copySign(vIntercept, wantedSpeed) : 0);
+  }
+
+  @Override
+  public TankDriveData apply(TankDriveData command) {
+    OpenLoopDriveSignal signal = getDriveSignal(
+        command.left.velocity.orElse(0),
+        command.left.acceleration.orElse(0),
+        command.right.velocity.orElse(0),
+        command.right.acceleration.orElse(0));
+
+    return new TankDriveData(
+        new DriveData(command.left.position,
+            OptionalDouble.empty(),
+            OptionalDouble.empty(),
+            OptionalDouble.of(signal.getLeftThrottle())),
+        new DriveData(command.right.position,
+            OptionalDouble.empty(),
+            OptionalDouble.empty(),
+            OptionalDouble.of(signal.getRightThrottle())),
+        command.heading,
+        command.turningRate);
+  }
+}
