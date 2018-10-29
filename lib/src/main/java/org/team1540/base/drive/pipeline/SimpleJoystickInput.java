d9c8e92077bb7ffb511410959d67389a9d22d8b4
Zachary Robinson <robinsonz@catlin.edu>
2018-10-29 11:00:51 -0700

    Add SimpleJoystickInput
    Needs to be documented

---
+++ lib/src/main/java/org/team1540/base/drive/pipeline/SimpleJoystickInput.java
@@ -0,0 +1,70 @@
+package org.team1540.base.drive.pipeline;
+
+import edu.wpi.first.wpilibj.Joystick;
+import java.util.OptionalDouble;
+import java.util.function.Supplier;
+import org.team1540.base.Utilities;
+
+public class SimpleJoystickInput implements Supplier<TankDriveData> {
+
+  private Joystick joystick;
+  private int leftAxis;
+  private int rightAxis;
+  private int fwdAxis;
+  private int backAxis; // or "baxis" if you don't have much time
+  private boolean invertLeft;
+  private boolean invertRight;
+
+  private double deadzone = 0.1;
+
+  @Override
+  public TankDriveData get() {
+    double triggerValue = Utilities.processDeadzone(joystick.getRawAxis(fwdAxis), deadzone)
+        - Utilities.processDeadzone(joystick.getRawAxis(backAxis), deadzone);
+    double leftThrottle = Utilities.constrain(
+        Utilities.processDeadzone(Utilities.invertIf(invertLeft, joystick.getRawAxis(leftAxis)),
+            deadzone) + triggerValue,
+        1
+    );
+    double rightThrottle = Utilities.constrain(
+        Utilities.processDeadzone(Utilities.invertIf(invertRight, joystick.getRawAxis(rightAxis)),
+            deadzone) + triggerValue,
+        1
+    );
+
+    return new TankDriveData(
+        new DriveData(
+            OptionalDouble.empty(),
+            OptionalDouble.empty(),
+            OptionalDouble.empty(),
+            OptionalDouble.of(leftThrottle)
+        ),
+        new DriveData(
+            OptionalDouble.empty(),
+            OptionalDouble.empty(),
+            OptionalDouble.empty(),
+            OptionalDouble.of(rightThrottle)
+        ),
+        OptionalDouble.empty(),
+        OptionalDouble.empty()
+    );
+  }
+
+  public SimpleJoystickInput(Joystick joystick, int leftAxis, int rightAxis, int fwdAxis,
+      int backAxis, boolean invertLeft, boolean invertRight) {
+    this(joystick, leftAxis, rightAxis, fwdAxis, backAxis, invertLeft, invertRight,
+        0.1);
+  }
+
+  public SimpleJoystickInput(Joystick joystick, int leftAxis, int rightAxis, int fwdAxis,
+      int backAxis, boolean invertLeft, boolean invertRight, double deadzone) {
+    this.joystick = joystick;
+    this.leftAxis = leftAxis;
+    this.rightAxis = rightAxis;
+    this.fwdAxis = fwdAxis;
+    this.backAxis = backAxis;
+    this.invertLeft = invertLeft;
+    this.invertRight = invertRight;
+    this.deadzone = deadzone;
+  }
+}
