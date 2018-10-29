1e790525ce74dc855e6e0cf72b447c1c10183fb5
Zachary Robinson <robinsonz@catlin.edu>
2018-10-29 16:12:00 -0700

    Integrate JoystickScaling into pipeline framework
    This is all backwards-compatible; just adds a default method which
    allows it to be used as part of drive pipelines.

--- lib/src/main/java/org/team1540/base/drive/JoystickScaling.java
+++ lib/src/main/java/org/team1540/base/drive/JoystickScaling.java
@@ -1,7 +1,13 @@
 package org.team1540.base.drive;
 
+import org.team1540.base.drive.pipeline.Processor;
+
 @FunctionalInterface
-public interface JoystickScaling {
+public interface JoystickScaling extends Processor<Double, Double> {
 
   public double scale(double input);
+
+  public default Double apply(Double input) {
+    return scale(input);
+  }
 }
