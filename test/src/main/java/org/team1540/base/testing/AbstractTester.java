aa0c3454602c99f343a66b618f81b4547cd340f2
edelmanjm <edelmanjm@users.noreply.github.com>
2018-11-01 17:14:34 -0700

    Add additional constructor, break out periodic

--- test/src/main/java/org/team1540/base/testing/AbstractTester.java
+++ test/src/main/java/org/team1540/base/testing/AbstractTester.java
@@ -1,6 +1,7 @@
 package org.team1540.base.testing;
 
 import com.google.common.collect.EvictingQueue;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
@@ -19,25 +20,31 @@ public abstract class AbstractTester<T, R> implements Tester<T, R> {
   @NotNull
   private List<Function<T, R>> tests;
   @NotNull
-  private List<T> itemsToTest;
+  List<T> itemsToTest;
   @NotNull
   private List<Supplier<Boolean>> runConditions;
   @NotNull
   private Map<T, Map<Function<T, R>, EvictingQueue<ResultWithMetadata<R>>>> allQueuedResults;
 
-  public AbstractTester(@NotNull List<Function<T, R>> tests, @NotNull List<T> itemsToTest,
+  AbstractTester(int numTests, @NotNull List<T> itemsToTest,
+      @NotNull List<Supplier<Boolean>> runConditions) {
+    realConstructor(new ArrayList<>(numTests), itemsToTest, runConditions,
+        (int) logTime / (updateDelay / 1000));
+  }
+
+  AbstractTester(@NotNull List<Function<T, R>> tests, @NotNull List<T> itemsToTest,
       @NotNull List<Supplier<Boolean>> runConditions) {
     realConstructor(tests, itemsToTest, runConditions, (int) logTime / (updateDelay / 1000));
   }
 
-  public AbstractTester(@NotNull List<Function<T, R>> tests, @NotNull List<T> itemsToTest,
+  AbstractTester(@NotNull List<Function<T, R>> tests, @NotNull List<T> itemsToTest,
       @NotNull List<Supplier<Boolean>> runConditions, float logTime, int updateDelay) {
     this.logTime = logTime;
     this.updateDelay = updateDelay;
     realConstructor(tests, itemsToTest, runConditions, (int) logTime / (updateDelay / 1000));
   }
 
-  public AbstractTester(@NotNull List<Function<T, R>> tests, @NotNull List<T> itemsToTest,
+  AbstractTester(@NotNull List<Function<T, R>> tests, @NotNull List<T> itemsToTest,
       @NotNull List<Supplier<Boolean>> runConditions, int queueDepth) {
     realConstructor(tests, itemsToTest, runConditions, queueDepth);
   }
@@ -94,16 +101,20 @@ public abstract class AbstractTester<T, R> implements Tester<T, R> {
     return allQueuedResults;
   }
 
+  void periodic() {
+    for (T t : itemsToTest) {
+      for (Function<T, R> test : tests) {
+        allQueuedResults.get(t).get(test).add(new ResultWithMetadata<>(test.apply(t),
+            System.currentTimeMillis()));
+      }
+    }
+  }
+
   @Override
   public void run() {
     while (true) {
 
-      for (T t : itemsToTest) {
-        for (Function<T, R> test : tests) {
-          allQueuedResults.get(t).get(test).add(new ResultWithMetadata<>(test.apply(t),
-              System.currentTimeMillis()));
-        }
-      }
+      periodic();
 
       try {
         Thread.sleep(updateDelay);
