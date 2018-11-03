d911037a02588280840831080afcacd6d87a6c30
edelmanjm <edelmanjm@users.noreply.github.com>
2018-11-02 18:49:32 -0700

    Make BurnoutTester sendable
    Because of the limitations of sendables, it can only send if a motor is burned out or not, but that should hopefully be enough.

--- test/src/main/java/org/team1540/base/testing/BurnoutTester.java
+++ test/src/main/java/org/team1540/base/testing/BurnoutTester.java
@@ -1,18 +1,24 @@
 package org.team1540.base.testing;
 
+import edu.wpi.first.wpilibj.Sendable;
+import edu.wpi.first.wpilibj.smartdashboard.SendableBuilder;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
 import org.apache.commons.math3.stat.descriptive.moment.StandardDeviation;
 import org.apache.commons.math3.stat.descriptive.rank.Median;
 import org.team1540.base.wrappers.ChickenTalon;
 
 @SuppressWarnings("unused")
-public class BurnoutTester extends AbstractTester<ChickenTalon, Boolean> {
+public class BurnoutTester extends AbstractTester<ChickenTalon, Boolean> implements Sendable {
 
   private static final Median medianCalculator = new Median();
   private static final StandardDeviation stdDevCalculator = new StandardDeviation();
   private double medianPower = 0;
   private double stdDevPower = 0;
+
+  private String name = "BurnoutTester";
 
   public BurnoutTester(List<ChickenTalon> motorsToTest) {
     // Because passing in a reference to a non-static method in the constructor doesn't work.
@@ -33,4 +39,35 @@ public class BurnoutTester extends AbstractTester<ChickenTalon, Boolean> {
     super.periodic();
   }
 
+  @Override
+  public String getName() {
+    return this.name;
+  }
+
+  @Override
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  @Override
+  public String getSubsystem() {
+    return this.name;
+  }
+
+  @Override
+  public void setSubsystem(String subsystem) {
+    this.name = subsystem;
+  }
+
+  @Override
+  public void initSendable(SendableBuilder builder) {
+    // Because the method reference by itself isn't an object apparently
+    Function<ChickenTalon, Boolean> lmao = this::testBurnout;
+    for (ChickenTalon t : getAllQueuedResults().keySet()) {
+      // Get the most recent value if present, else return false
+      builder.addBooleanProperty(t.toString(),
+          () -> Optional.ofNullable(getAllQueuedResults().get(t).get(lmao).peek())
+              .map(ResultWithMetadata::getResult).orElse(false), null);
+    }
+  }
 }
