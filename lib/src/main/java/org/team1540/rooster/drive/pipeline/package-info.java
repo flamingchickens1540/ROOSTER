5c20f1869bfba624456ce631681f679074d17aa3
Zachary Robinson <robinsonz@catlin.edu>
2018-11-26 15:56:17 -0800

    Add note about nullity to package-info.java

--- lib/src/main/java/org/team1540/rooster/drive/pipeline/package-info.java
+++ lib/src/main/java/org/team1540/rooster/drive/pipeline/package-info.java
@@ -4,5 +4,8 @@
  * org.team1540.rooster.drive.pipeline.Processor Processor}, and {@link
  * org.team1540.rooster.drive.pipeline.Output Output} interfaces (as well as those interfaces
  * themselves).
+ *
+ * <p>Unless otherwise noted, all methods, constructors, etc. in this package throw a {@link
+ * java.lang.NullPointerException} if any parameters are {@code null}.
  */
 package org.team1540.rooster.drive.pipeline;
