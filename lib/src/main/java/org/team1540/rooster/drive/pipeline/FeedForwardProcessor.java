7380f93dc4f75ad3baa1a0c6a3408c19f47c4913
Zachary Robinson <robinsonz@catlin.edu>
2018-11-26 16:39:31 -0800

    Document FeedForwardProcessor

--- lib/src/main/java/org/team1540/rooster/drive/pipeline/FeedForwardProcessor.java
+++ lib/src/main/java/org/team1540/rooster/drive/pipeline/FeedForwardProcessor.java
@@ -2,7 +2,14 @@ package org.team1540.rooster.drive.pipeline;
 
 import java.util.OptionalDouble;
 import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
 
+/**
+ * {@link Processor} to apply an <a href="https://www.chiefdelphi.com/media/papers/3402">Oblarg-style</a>
+ * feed-forward. This processor allows for velocity, acceleration, and static friction (vIntercept)
+ * feed-forwards. For further details, see {@link #apply(TankDriveData) apply()}. The output will
+ * mirror the input with the exception of a change in the feed-forward value.
+ */
 public class FeedForwardProcessor implements Processor<TankDriveData, TankDriveData> {
 
   private final double kV;
@@ -29,8 +36,25 @@ public class FeedForwardProcessor implements Processor<TankDriveData, TankDriveD
         + (wantedSpeed != 0 ? Math.copySign(vIntercept, wantedSpeed) : 0);
   }
 
+  /**
+   * Applys feed-forwards to the provided {@link TankDriveData}. The method for calculating the
+   * feed-forward is as follows:
+   * <ol>
+   * <li>The feed-forward starts at 0. </li>
+   * <li>The product of the velocity (if present) and the kV is added.</li>
+   * <li>The product of the acceleration (if present) and the kA is added.</li>
+   * <li>If the velocity is present and nonzero, the velocity intercept (with the sign of the
+   * velocity) is added.</li>
+   * </ol>
+   * The calculated feed-forward is then added to any feed-forward already present in the
+   * DriveData.
+   *
+   * @param command The data to use.
+   * @return A new {@link TankDriveData} as described above.
+   */
   @Override
-  public TankDriveData apply(TankDriveData command) {
+  @NotNull
+  public TankDriveData apply(@NotNull TankDriveData command) {
     return new TankDriveData(
         new DriveData(command.left.position,
             command.left.velocity,
