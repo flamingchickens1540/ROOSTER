e5b892c32e7c89a6c905b93fd930c8514db02b40
Zachary Robinson <robinsonz@catlin.edu>
2018-11-30 16:58:10 -0800

    Extract PID logic from TurningRateClosedLoopProcessor
    Also documentation!

---
+++ lib/src/main/java/org/team1540/rooster/drive/pipeline/PIDProcessor.java
@@ -0,0 +1,113 @@
+package org.team1540.rooster.drive.pipeline;
+
+/**
+ * Processor to execute a generic PID loop.
+ *
+ * @param <I> The input type of the processor.
+ * @param <O> The output type of the processor.
+ */
+public abstract class PIDProcessor<I, O> implements Processor<I, O> {
+
+  private double p, i, d;
+
+  private double iAccum;
+
+  private double lastError;
+
+  private long lastTime = -1;
+
+  /**
+   * Create a new {@code PIDProcessor}.
+   *
+   * @param p The P coefficient.
+   * @param i The I coefficient.
+   * @param d The D coefficient.
+   */
+  protected PIDProcessor(double p, double i, double d) {
+    this.p = p;
+    this.i = i;
+    this.d = d;
+  }
+
+  /**
+   * Extract the PID loop target from the data passed to the processor, and calculate the current
+   * error.
+   *
+   * @param data The data instance that was passed to the processor's {@link #apply(Object) apply()}
+   * method.
+   * @return The error.
+   */
+  protected abstract double getError(I data);
+
+  /**
+   * Create the processor output.
+   *
+   * @param data The data instance that was passed to the processor's {@link #apply(Object) apply()}
+   * method.
+   * @param loopOutput The output of the PID loop
+   * @return The processor's output; this will be returned from the the processor's {@link
+   * #apply(Object) apply()} method.
+   */
+  protected abstract O createOutput(I data, double loopOutput);
+
+  @Override
+  public O apply(I input) {
+    double output = 0;
+    double error = getError(input);
+
+    // p gain
+    output += error * p;
+
+    if (lastTime != -1) {
+      double dt = (System.currentTimeMillis() - lastTime) / 1000.0;
+
+      // i gain
+      iAccum += error * dt;
+      output += i * iAccum;
+
+      // d gain
+      double dError = (error - lastError) / dt;
+      output += d * dError;
+    }
+
+    lastError = error;
+    lastTime = System.currentTimeMillis();
+
+    return createOutput(input, output);
+  }
+
+
+  /**
+   * Gets the current value of the integral accumulator.
+   *
+   * @return The integral accumulator. If the PID loop has not yet been run (i.e. {@link
+   * #apply(Object) apply()} has not yet been called since instantiation/the last call to {@link
+   * #reset()}), returns 0.
+   */
+  public double getIAccum() {
+    return iAccum;
+  }
+
+  /**
+   * Gets the closed-loop error from the last run of the PID loop.
+   *
+   * @return The closed-loop error. If the PID loop has not yet been run (i.e. {@link #apply(Object)
+   * apply()} has not yet been called since instantiation/the last call to {@link #reset()}),
+   * returns 0.
+   */
+  public double getError() {
+    return lastError;
+  }
+
+  /**
+   * Resets the PID loop. This clears the integral accumulator and error values, and should be
+   * called if the {@code TurningRateClosedLoopProcessor} is being reused for multiple discrete
+   * occasions (i.e. executions of different motion profile segments). Calling this is functionally
+   * equivalent to creating a new processor instance.
+   */
+  public void reset() {
+    iAccum = 0;
+    lastError = 0;
+    lastTime = -1;
+  }
+}
---
+++ lib/src/main/java/org/team1540/rooster/drive/pipeline/TurningRatePIDProcessor.java
@@ -0,0 +1,132 @@
+package org.team1540.rooster.drive.pipeline;
+
+import java.util.Objects;
+import java.util.OptionalDouble;
+import java.util.function.DoubleSupplier;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Modifies velocity set-points to keep a desired turning rate. This {@link Processor} accepts a
+ * {@link TankDriveData} and uses the {@linkplain TankDriveData#turningRate turning rate} as an
+ * input to the loop.
+ *
+ * @see Processor
+ * @see PIDProcessor
+ */
+public class TurningRatePIDProcessor extends PIDProcessor<TankDriveData, TankDriveData> {
+
+  private DoubleSupplier yawRateSupplier;
+  private boolean invertSides;
+
+  @Override
+  protected double getError(TankDriveData data) {
+    return data.turningRate.isPresent() ? data.turningRate.getAsDouble() - yawRateSupplier
+        .getAsDouble() : 0;
+  }
+
+  @Override
+  protected TankDriveData createOutput(TankDriveData data, double loopOutput) {
+    // multiplying the output by -1 effectively flips the sides
+    loopOutput *= invertSides ? -1 : 1;
+
+    return new TankDriveData(
+        new DriveData(
+            data.left.position,
+            OptionalDouble.of(data.left.velocity.orElse(0) - loopOutput),
+            data.left.acceleration,
+            data.left.additionalFeedForward
+        ),
+        new DriveData(
+            data.right.position,
+            OptionalDouble.of(data.right.velocity.orElse(0) + loopOutput),
+            data.right.acceleration,
+            data.right.additionalFeedForward
+        ),
+        data.heading,
+        data.turningRate
+    );
+  }
+
+  /**
+   * Creates a new {@code TurningRateClosedLoopProcessor} with P and I D coefficients of 0 that does
+   * not invert sides.
+   *
+   * @param yawRateSupplier A {@link DoubleSupplier} that supplies the current yaw rate in radians
+   * per second.
+   * @param p The P coefficient of the PID loop.
+   */
+  public TurningRatePIDProcessor(DoubleSupplier yawRateSupplier, double p) {
+    this(yawRateSupplier, p, 0);
+  }
+
+  /**
+   * Creates a new {@code TurningRateClosedLoopProcessor} with a D coefficient of 0 that does not
+   * invert sides.
+   *
+   * @param yawRateSupplier A {@link DoubleSupplier} that supplies the current yaw rate in radians
+   * per second.
+   * @param p The P coefficient of the PID loop.
+   * @param i The I coefficient of the PID loop.
+   */
+  public TurningRatePIDProcessor(DoubleSupplier yawRateSupplier, double p, double i) {
+    this(yawRateSupplier, p, i, 0);
+  }
+
+  /**
+   * Creates a new {@code TurningRateClosedLoopProcessor} that does not invert sides.
+   *
+   * @param yawRateSupplier A {@link DoubleSupplier} that supplies the current yaw rate in radians
+   * per second.
+   * @param p The P coefficient of the PID loop.
+   * @param i The I coefficient of the PID loop.
+   * @param d The D coefficient of the PID loop.
+   */
+  public TurningRatePIDProcessor(DoubleSupplier yawRateSupplier, double p, double i,
+      double d) {
+    this(yawRateSupplier, p, i, d, false);
+  }
+
+  /**
+   * Creates a new {@code TurningRateClosedLoopProcessor} with I and D coefficients of 0.
+   *
+   * @param yawRateSupplier A {@link DoubleSupplier} that supplies the current yaw rate in radians
+   * per second.
+   * @param p The P coefficient of the PID loop.
+   * @param invertSides Whether to invert the output of the PID loop before sending it onwards.
+   */
+  public TurningRatePIDProcessor(DoubleSupplier yawRateSupplier, double p,
+      boolean invertSides) {
+    this(yawRateSupplier, p, 0, invertSides);
+  }
+
+  /**
+   * Creates a new {@code TurningRateClosedLoopProcessor} with a D coefficient of 0.
+   *
+   * @param yawRateSupplier A {@link DoubleSupplier} that supplies the current yaw rate in radians
+   * per second.
+   * @param p The P coefficient of the PID loop.
+   * @param i The I coefficient of the PID loop.
+   * @param invertSides Whether to invert the output of the PID loop before sending it onwards.
+   */
+  public TurningRatePIDProcessor(DoubleSupplier yawRateSupplier, double p, double i,
+      boolean invertSides) {
+    this(yawRateSupplier, p, i, 0, invertSides);
+  }
+
+  /**
+   * Creates a new {@code TurningRateClosedLoopProcessor}.
+   *
+   * @param yawRateSupplier A {@link DoubleSupplier} that supplies the current yaw rate in radians
+   * per second.
+   * @param p The P coefficient of the PID loop.
+   * @param i The I coefficient of the PID loop.
+   * @param d The D coefficient of the PID loop.
+   * @param invertSides Whether to invert the output of the PID loop before sending it onwards.
+   */
+  public TurningRatePIDProcessor(@NotNull DoubleSupplier yawRateSupplier, double p, double i,
+      double d, boolean invertSides) {
+    super(p, i, d);
+    this.yawRateSupplier = Objects.requireNonNull(yawRateSupplier);
+    this.invertSides = invertSides;
+  }
+}
