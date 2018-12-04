922ffe42899a18a06998c227d6c50460c356720e
edelmanjm <edelmanjm@users.noreply.github.com>
2018-12-03 16:12:03 -0800

    Refactor IMotorControllers to ChickenTalons
    Does this lose a little bit of functionality? Yes. Does it also make things otherwise massively nicer all around? Also yes.

--- lib/src/main/java/org/team1540/rooster/testers/motor/EncoderTester.java
+++ lib/src/main/java/org/team1540/rooster/testers/motor/EncoderTester.java
@@ -1,5 +1,6 @@
 package org.team1540.rooster.testers.motor;
 
+import com.ctre.phoenix.motorcontrol.IMotorController;
 import edu.wpi.first.wpilibj.Sendable;
 import edu.wpi.first.wpilibj.smartdashboard.SendableBuilder;
 import java.util.Arrays;
@@ -14,7 +15,7 @@ import org.team1540.rooster.testers.ResultWithMetadata;
  * and if the corresponding encoder is moving.
  */
 @SuppressWarnings("unused")
-public class EncoderTester extends AbstractTester<ChickenTalon, Boolean> implements Sendable {
+public class EncoderTester extends AbstractTester<IMotorController, Boolean> implements Sendable {
 
   private String name = "EncoderTester";
 
@@ -26,19 +27,19 @@ public class EncoderTester extends AbstractTester<ChickenTalon, Boolean> impleme
    * ms. Equivalent to {@link EncoderTester#EncoderTester(List) EncoderTester(Arrays.asList
    * (motorsToTest))}.
    *
-   * @param motorsToTest The {@link ChickenTalon ChickenTalons} to compare to each other.
+   * @param motorsToTest The {@link IMotorController IMotorControllers} to compare to each other.
    */
-  public EncoderTester(ChickenTalon... motorsToTest) {
+  public EncoderTester(IMotorController... motorsToTest) {
     this(Arrays.asList(motorsToTest));
   }
 
   /**
    * Construct a new instance with the default logTime of 150 seconds and an update delay of 500 ms.
    *
-   * @param motorsToTest The {@link ChickenTalon ChickenTalons} to compare to each other.
+   * @param motorsToTest The {@link IMotorController IMotorControllers} to compare to each other.
    */
   @SuppressWarnings("WeakerAccess")
-  public EncoderTester(List<ChickenTalon> motorsToTest) {
+  public EncoderTester(List<IMotorController> motorsToTest) {
     // Because passing in a reference to a non-static method in the constructor doesn't work.
     // Test will run if the motor is drawing over 1A of current.
     super((stupid) -> null, motorsToTest, null, 150, 500);
@@ -49,16 +50,17 @@ public class EncoderTester extends AbstractTester<ChickenTalon, Boolean> impleme
 
   /**
    * Tests to see if the encoder is working by checking to see if the controller is drawing more
-   * than 1 amp and if the selected {@link ChickenTalon} is moving at a velocity of less than 5.
+   * than 1 amp and if the selected {@link IMotorController} is moving at a velocity of less than 5.
    *
-   * @param controller The {@link ChickenTalon} to test for burnout.
+   * @param controller The {@link IMotorController} to test for burnout.
    * @return Boolean indicating if the encoder is encoder has failed: true if it is not suspected
    * of failure, false if it is suspected of failure.
    */
   @SuppressWarnings("WeakerAccess")
-  public Boolean testEncoder(ChickenTalon controller) {
+  public Boolean testEncoder(IMotorController controller) {
+    // Do the wrappers provide pidIdx nicely? Yes. Can we just use zero? Also probably yes.
     return !(controller.getOutputCurrent() > currentThreshold
-        && controller.getSelectedSensorVelocity() < velocityThreshold);
+        && controller.getSelectedSensorVelocity(0) < velocityThreshold);
   }
 
   @Override
@@ -118,13 +120,13 @@ public class EncoderTester extends AbstractTester<ChickenTalon, Boolean> impleme
   }
 
   /**
-   * Displays the current status of each {@link ChickenTalon}.
+   * Displays the current status of each {@link IMotorController}.
    *
    * @param builder The {@link SendableBuilder} to use.
    */
   @Override
   public void initSendable(SendableBuilder builder) {
-    for (ChickenTalon t : getItemsToTest()) {
+    for (IMotorController t : getItemsToTest()) {
       // Get the most recent value if present, else simply don't add it to the builder
       //noinspection Duplicates
       builder.addBooleanProperty(t.getDeviceID() + "", () -> {
