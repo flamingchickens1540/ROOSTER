1237c4506d485e2e8e4de1b58f02dcda78a92445
edelmanjm <edelmanjm@users.noreply.github.com>
2018-12-03 16:11:16 -0800

    Add better support for single motor testing
    Can now check using cutoffs instead of comparing against other motors in the group.
    Also includes accessors for the relevant fields.

--- lib/src/main/java/org/team1540/rooster/testers/motor/BurnoutTester.java
+++ lib/src/main/java/org/team1540/rooster/testers/motor/BurnoutTester.java
@@ -13,7 +13,7 @@ import org.team1540.rooster.testers.ResultWithMetadata;
 
 /**
  * Reports motor burnouts by comparing the current draw across a series of similarly-purposed
- * motors and reporting low outliers.
+ * motors and reporting low outliers or checking if a single motor is below the cutoff.
  */
 @SuppressWarnings("unused")
 public class BurnoutTester extends AbstractTester<IMotorController, Boolean> implements Sendable {
@@ -23,12 +23,17 @@ public class BurnoutTester extends AbstractTester<IMotorController, Boolean> imp
   private double medianCurrent = 0;
   private double stdDevCurrent = 0;
 
+  private double currentCutoff = 1;
+  private double percentOutputCutoff = 0.5;
+
   private String name = "BurnoutTester";
 
   /**
    * Construct a new instance with the default logTime of 150 seconds and an update delay of 500
-   * ms. Equivalent to {@link BurnoutTester#BurnoutTester(List) EncoderTester(Arrays.asList
-   * (motorsToTest))}.
+   * ms, using the {@link BurnoutTester#testBurnoutMultiMotor(IMotorController)} if there is more
+   * than one motor and {@link BurnoutTester#testBurnoutSingleMotor(IMotorController)} if there is
+   * one or few motors. Equivalent to {@link BurnoutTester#BurnoutTester(List) EncoderTester
+   * (Arrays.asList(motorsToTest))}.
    *
    * @param motorsToTest The motors to compare to each other.
    */
@@ -38,7 +43,10 @@ public class BurnoutTester extends AbstractTester<IMotorController, Boolean> imp
   }
 
   /**
-   * Construct a new instance with the default logTime of 150 seconds and an update delay of 500 ms.
+   * Construct a new instance with the default logTime of 150 seconds and an update delay of 500
+   * ms, using the {@link BurnoutTester#testBurnoutMultiMotor(IMotorController)} if there are more
+   * than two motor sand {@link BurnoutTester#testBurnoutSingleMotor(IMotorController)} if there two
+   * two or few motors.
    *
    * @param motorsToTest The motors to compare to each other.
    */
@@ -46,26 +54,40 @@ public class BurnoutTester extends AbstractTester<IMotorController, Boolean> imp
   public BurnoutTester(List<IMotorController> motorsToTest) {
     // Because passing in a reference to a non-static method in the constructor doesn't work.
     super((stupid) -> null, motorsToTest, null, 150, 500);
-    this.setTest(this::testBurnout);
+    this.setTest(motorsToTest.size() > 2 ? this::testBurnoutMultiMotor :
+        this::testBurnoutSingleMotor);
   }
 
   /**
-   * Tests to see if a motor is burned out by checking to see if it is at least one standard
-   * deviation below the median.
+   * Tests to see if a motor is burned out by checking to see if the current being drawn is at
+   * least one standard deviation below the median.
    * @param controller The motor to test for burnout.
    * @return Boolean indicating burnout.
    */
   @SuppressWarnings("WeakerAccess")
-  public Boolean testBurnout(IMotorController controller) {
+  public boolean testBurnoutMultiMotor(IMotorController controller) {
     return controller.getOutputCurrent() < (this.medianCurrent - 1 * this.stdDevCurrent);
+  }
+
+  /**
+   * Test to see if a motor is burned out by checking to see if the current being drawn is below
+   * the current cutoff.
+   *
+   * @param controller The motor to test for burnout.
+   * @return Boolean indicating burnout.
+   */
+  @SuppressWarnings("WeakerAccess")
+  public boolean testBurnoutSingleMotor(IMotorController controller) {
+    return controller.getMotorOutputPercent() > percentOutputCutoff
+        && controller.getOutputCurrent() < currentCutoff;
   }
 
   /**
    * Gets the currents, calculates the median and standard deviation, then calls super.
    */
   @Override
-  void periodic() {
-    double[] currents = itemsToTest.stream().mapToDouble(IMotorController::getOutputCurrent)
+  protected void periodic() {
+    double[] currents = getItemsToTest().stream().mapToDouble(IMotorController::getOutputCurrent)
         .toArray();
     medianCurrent = medianCalculator.evaluate(currents);
     stdDevCurrent = stdDevCalculator.evaluate(currents);
@@ -90,6 +112,42 @@ public class BurnoutTester extends AbstractTester<IMotorController, Boolean> imp
   @Override
   public void setSubsystem(String subsystem) {
     this.name = subsystem;
+  }
+
+  /**
+   * Gets the current cutoff. Defaults to 1A.
+   *
+   * @return The current cutoff in amps.
+   */
+  public double getCurrentCutoff() {
+    return currentCutoff;
+  }
+
+  /**
+   * Sets the current cutoff.
+   *
+   * @param currentCutoff The current cutoff in amps.
+   */
+  public void setCurrentCutoff(double currentCutoff) {
+    this.currentCutoff = currentCutoff;
+  }
+
+  /**
+   * Gets the percent output cutoff. Defaults to 50%.
+   *
+   * @return The percent output cutoff as a percentage.
+   */
+  public double getPercentOutputCutoff() {
+    return percentOutputCutoff;
+  }
+
+  /**
+   * Sets the percent output cutoff.
+   *
+   * @param percentOutputCutoff The output cutoff as a percentage.
+   */
+  public void setPercentOutputCutoff(double percentOutputCutoff) {
+    this.percentOutputCutoff = percentOutputCutoff;
   }
 
   /**
